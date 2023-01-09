475af6aa07df28796f499ecbbf057c86ee7a1622
Mainu <mainu.co.cc@gmail.com>
2023-01-09 22:52:15 +0600

    :tada: initial commit

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default function Main() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to Jenkins</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
